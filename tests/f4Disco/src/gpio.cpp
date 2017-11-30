a8258ab94b032158f8830c5b3c8775678220e40a
Kizarm <mrazik@volny.cz>
2017-11-30 16:57:26 +0100

    priklad lze prelozit i clang-3.8

--- tests/f4Disco/src/gpio.cpp
+++ tests/f4Disco/src/gpio.cpp
@@ -1,5 +1,6 @@
 #include "stm32f40x.h"
 #include "gpio.h"
+#if 0
 // Tohle je nutné vytáhnout ze staticky generované hlavičky.
 static const uint32_t RCC_AHB1ENR_GPIOAEN = 0x00000001u; /*!< Ofs=0 IO port A clock enable */
 static const uint32_t RCC_AHB1ENR_GPIOBEN = 0x00000002u; /*!< Ofs=1 IO port B clock enable */
@@ -20,10 +21,33 @@ static GPIO_Type * const pGPIOF = (GPIO_Type * const) 0x40021400u; /*!< General-
 static GPIO_Type * const pGPIOG = (GPIO_Type * const) 0x40021800u; /*!< General-purpose I/Os */
 static GPIO_Type * const pGPIOH = (GPIO_Type * const) 0x40021C00u; /*!< General-purpose I/Os */
 static GPIO_Type * const pGPIOI = (GPIO_Type * const) 0x40022000u; /*!< General-purpose I/Os */
+#endif // 0
 
+#define RCC_AHB1ENR_GPIOAEN ((uint32_t)0x00000001u) /*!< Ofs=0 IO port A clock enable */
+#define RCC_AHB1ENR_GPIOBEN ((uint32_t)0x00000002u) /*!< Ofs=1 IO port B clock enable */
+#define RCC_AHB1ENR_GPIOCEN ((uint32_t)0x00000004u) /*!< Ofs=2 IO port C clock enable */
+#define RCC_AHB1ENR_GPIODEN ((uint32_t)0x00000008u) /*!< Ofs=3 IO port D clock enable */
+#define RCC_AHB1ENR_GPIOEEN ((uint32_t)0x00000010u) /*!< Ofs=4 IO port E clock enable */
+#define RCC_AHB1ENR_GPIOFEN ((uint32_t)0x00000020u) /*!< Ofs=5 IO port F clock enable */
+#define RCC_AHB1ENR_GPIOGEN ((uint32_t)0x00000040u) /*!< Ofs=6 IO port G clock enable */
+#define RCC_AHB1ENR_GPIOHEN ((uint32_t)0x00000080u) /*!< Ofs=7 IO port H clock enable */
+#define RCC_AHB1ENR_GPIOIEN ((uint32_t)0x00000100u) /*!< Ofs=8 IO port I clock enable */
+
+#define pGPIOA ((GPIO_Type * const) 0x40020000u) /*!< General-purpose I/Os */
+#define pGPIOB ((GPIO_Type * const) 0x40020400u) /*!< General-purpose I/Os */
+#define pGPIOC ((GPIO_Type * const) 0x40020800u) /*!< General-purpose I/Os */
+#define pGPIOD ((GPIO_Type * const) 0x40020C00u) /*!< General-purpose I/Os */
+#define pGPIOE ((GPIO_Type * const) 0x40021000u) /*!< General-purpose I/Os */
+#define pGPIOF ((GPIO_Type * const) 0x40021400u) /*!< General-purpose I/Os */
+#define pGPIOG ((GPIO_Type * const) 0x40021800u) /*!< General-purpose I/Os */
+#define pGPIOH ((GPIO_Type * const) 0x40021C00u) /*!< General-purpose I/Os */
+#define pGPIOI ((GPIO_Type * const) 0x40022000u) /*!< General-purpose I/Os */
 // Tabulka 72 bytu ve flash, kód to zkrátí i zrychlí. Aby byla tabulka ve flash, nelze do ní
 // umístit odkazy, musí to být ukazatele (jsou konstantní). V C to takhle inicializovat nejde,
-// musely by se použít define, C++ to spolkne i statické konstanty.
+// musely by se použít define, C++ (gcc) to spolkne i statické konstanty.
+// Ukázalo se, že clang sice statické konstanty spolkne, ale umístí tabulku do .bss (vytvoří
+// pro ní statický kostruktor). To je sice nepříjemné, ale horší je, že to nefunguje.
+// Pokud zůstaneme u klasických define, funguje to správně, takže to tak necháme.
 static const struct GpioAssocPort cPortTab[] = {
   {pGPIOA, RCC_AHB1ENR_GPIOAEN},
   {pGPIOB, RCC_AHB1ENR_GPIOBEN},
